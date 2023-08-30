4bfff30bfed6a3ae12803a28d7b7d9abeff6acb5
Adhicitta Masran <adhicittamasran@gmail.com>
2023-08-30 20:37:36 +0800

    company: add validation to company request

--- request/company.go
+++ request/company.go
@@ -14,7 +14,14 @@ type (
 	}
 )
 
-// TODO: tuliskan validasi untuk CompanyRequest dengan rule semua field required
+func (req CompanyRequest) Validate() error {
+	return validation.ValidateStruct(
+		&req,
+		validation.Field(&req.Name, validation.Required),
+		validation.Field(&req.Balance, validation.Required),
+		validation.Field(&req.Address, validation.Required),
+	)
+}
 
 func (req TopupCompanyBalance) Validate() error {
 	return validation.ValidateStruct(
