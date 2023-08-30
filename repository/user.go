2e394018d8d77da11a3bf668820815d838591751
Adhicitta Masran <adhicittamasran@gmail.com>
2023-08-30 21:22:04 +0800

    user: implement FindByID, Create, and UpdateById repo

--- repository/user.go
+++ repository/user.go
@@ -15,25 +15,36 @@ func NewUserRepository(cfg config.Config) model.UserRepository {
 }
 
 func (p *userRepository) FindByID(ctx context.Context, id int) (*model.User, error) {
+	user := new(model.User)
 
-	// TODO: buat fungsi untuk mencari user berdasarkan ID pada parameter
+	if err := p.Cfg.Database().WithContext(ctx).Preload("Position").First(user, id).Error; err != nil {
+		return nil, err
+	}
 
-	panic("implement me ")
-
+	return user, nil
 }
 
 func (p *userRepository) Create(ctx context.Context, user *model.User) (*model.User, error) {
-	// TODO: buat fungsi untuk membuat user berdasarkan struct parameter
+	if err := p.Cfg.Database().WithContext(ctx).Create(user).Error; err != nil {
+		return nil, err
+	}
 
-	panic("implement me ")
+	return user, nil
 
 }
 
 func (p *userRepository) UpdateByID(ctx context.Context, id int, user *model.User) (*model.User, error) {
-	// TODO: buat fungsi untuk update user berdasarkan struct parameter
+	_, err := p.FindByID(ctx, id)
 
-	panic("implement me ")
+	if err != nil {
+		return nil, err
+	}
 
+	if err := p.Cfg.Database().WithContext(ctx).Where("id", id).Updates(user).Find(user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (p *userRepository) Delete(ctx context.Context, id int) error {
